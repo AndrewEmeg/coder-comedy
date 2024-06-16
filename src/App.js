02c8360a968b61d2e2446e30ff41bd32a81580e3
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2024-06-16 14:16:09 -0400

    Added the required images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
-}
+  const [joke, setJoke] = useState("This is where the joke should appear.");
+  // const [amount, setAmount] = useState(4);
 
+  async function getJoke() {
+    const res = await fetch(
+      `https://v2.jokeapi.dev/joke/Programming?blacklistFlags=sexist`
+    );
+    const prom = await res.json();
+
+    if (Boolean(prom.joke) === true) {
+      console.log("single");
+      setJoke(prom.joke);
+    } else {
+      console.log("two-part");
+      setJoke(`${prom.setup}  ${prom.delivery}`);
+    }
+
+    console.log(prom);
+    // console.log(prom.joke || prom.setup, prom.delivery);
+  }
+  return (
+    <div className="App">
+      {/* <select value={amount} onChange={(e) => setAmount(e.target.value)}>
+        {Array.from({ length: 5 }, (_, i) => i + 1).map((number) => (
+          <option key={number}>{number}</option>
+        ))}
+      </select> */}
+      <p>{joke}</p>
+      <button className="get-joke" onClick={getJoke}>
+        Get Jokee!
+      </button>
+      <p>What's happening</p>
+    </div>
+  );
+}
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <ul>
+          <li className="nav-element">Home</li>
+          <li className="nav-element">About</li>
+          <li className="nav-element">Developer</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 export default App;
