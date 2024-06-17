9652ca54b849fa68e3740d05820ae1568a64100b
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2024-06-17 01:23:43 -0400

    Added New Hero Images for testing and more code too.

--- src/App.js
+++ src/App.js
@@ -2,43 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [joke, setJoke] = useState("This is where the joke should appear.");
-  // const [amount, setAmount] = useState(4);
-
-  async function getJoke() {
-    const res = await fetch(
-      `https://v2.jokeapi.dev/joke/Programming?blacklistFlags=sexist`
-    );
-    const prom = await res.json();
-
-    if (Boolean(prom.joke) === true) {
-      console.log("single");
-      setJoke(prom.joke);
-    } else {
-      console.log("two-part");
-      setJoke(`${prom.setup}  ${prom.delivery}`);
-    }
-
-    console.log(prom);
-    // console.log(prom.joke || prom.setup, prom.delivery);
-  }
   return (
     <div className="App">
       <Header />
       <Hero />
-      {/* <select value={amount} onChange={(e) => setAmount(e.target.value)}>
-        {Array.from({ length: 5 }, (_, i) => i + 1).map((number) => (
-          <option key={number}>{number}</option>
-        ))}
-      </select> */}
-      <p>{joke}</p>
-      <button className="get-joke" onClick={getJoke}>
-        Get Jokee!
-      </button>
-      <p>What's happening</p>
+      <AppOther />
     </div>
   );
 }
+
 function Header() {
   return (
     <header className="header">
@@ -112,6 +84,94 @@ function Hero() {
         </div>
       </div>
       <div className="hero-empty"></div>
+    </div>
+  );
+}
+
+function AppOther() {
+  return (
+    <>
+      <TopJokes />
+      <GenerateJoke />
+    </>
+  );
+}
+
+function TopJokes() {
+  return (
+    <div className="container  jokes-featured">
+      <h2 className="heading-h2">Today's Top Jokes</h2>
+      <div className="funny-jokes">
+        <div className="jokes-featured-card">
+          <img
+            src="./images/besties.jpg"
+            alt=""
+            className="jokes-featured-card-img"
+          />
+          <p className="jokes-featured-card-paragraph">
+            “Why are Assembly programmers always soaking wet? They work below
+            C-level.”
+          </p>
+        </div>
+        <div className="jokes-featured-card">
+          <img
+            src="./images/buddies.jpg"
+            alt=""
+            className="jokes-featured-card-img"
+          />
+          <p className="jokes-featured-card-paragraph">
+            “Why do Java developers wear glasses? Because they don't C#.”
+          </p>
+        </div>
+        <div className="jokes-featured-card">
+          <img
+            src="./images/solo-prenuer.jpg"
+            alt=""
+            className="jokes-featured-card-img"
+          />
+          <p className="jokes-featured-card-paragraph">
+            “Why do programmers prefer dark mode? Because light attracts bugs.”
+          </p>
+        </div>
+      </div>
+      <h2 className="heading-h2">
+        Go ahead, laugh all
+        <br /> the more!
+      </h2>
+      <p className="hero-subheadline">
+        Click on the “Generate Joke” button below to crack your ribsssss
+        🤣🤣🤣🤣
+      </p>
+    </div>
+  );
+}
+
+function GenerateJoke() {
+  const [joke, setJoke] = useState('"This is where the joke should appear."');
+
+  async function getJoke() {
+    const res = await fetch(
+      `https://v2.jokeapi.dev/joke/Programming?blacklistFlags=sexist`
+    );
+    const prom = await res.json();
+
+    if (Boolean(prom.joke) === true) {
+      console.log("single");
+      setJoke(prom.joke);
+    } else {
+      console.log("two-part");
+      setJoke(`${prom.setup}  ${prom.delivery}`);
+    }
+  }
+  return (
+    <div className="generate-joke">
+      <div className="container">
+        <h2 className="hero-subheadline">{joke}</h2>
+        <button className="get-joke" onClick={getJoke}>
+          Get Jokee!
+        </button>
+        <p>What's happening</p>
+      </div>
     </div>
   );
 }
